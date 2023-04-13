5b31485ac07381e55dafc2314901917562d7293a
Marina Matulle <marinamatulle.mm@gmail.com>
2023-04-12 23:09:21 -0300

    Lista 6 da aula de POO da faculdade

---
+++ exercicio-4/src/TesteProtegido.java
@@ -0,0 +1,9 @@
+public class TesteProtegido {
+    public static void main(String[] args) {
+        Protegido exemplo = new Protegido(8);
+        System.out.println("x: "+ exemplo.getI());
+        ProtegidoSub exemplo1 = new ProtegidoSub(4);
+        exemplo1.mudar(12);
+        System.out.println("x: "+ exemplo1.getI());
+    }
+}
